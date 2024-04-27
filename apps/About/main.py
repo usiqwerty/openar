338b26b32d42544c3564a71645e5ae081297591f
usiqwerty <benlogin1@protonmail.com>
2024-04-27 17:42:11 +0500

    render() реализован по умолчанию

--- apps/About/main.py
+++ apps/About/main.py
@@ -2,16 +2,9 @@ import numpy as np
 
 from gui.abstract.app import Application
 from gui.elements.text import Text
-from video.rendering import overlay_images
 
 
 class App(Application):
-    def render(self) -> np.ndarray:
-        for (x, y, element) in self.elements:
-            overlay_images(self.frame, element.draw(), x, y)
-
-        return self.frame
-
     def on_start(self):
         self.elements.append((0, 100, Text("The About App", font_size=3)))
 
@@ -19,5 +12,6 @@ class App(Application):
         super().__init__()
         self.name = "About"
         self.size = (500, 700)
+        self.position = (0, 100)
         self.frame = np.full((*self.size, 4), (0, 255, 255, 255))
         self.elements = []
