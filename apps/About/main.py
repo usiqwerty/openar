d2d5128165b8a42b6b1dbef03b9fc1bd8df83023
usiqwerty <benlogin1@protonmail.com>
2024-04-27 17:46:56 +0500

    Упрощённое объявление приложения

--- apps/About/main.py
+++ apps/About/main.py
@@ -5,13 +5,14 @@ from gui.elements.text import Text
 
 
 class App(Application):
-    def on_start(self):
-        self.elements.append((0, 100, Text("The About App", font_size=3)))
-
     def __init__(self):
         super().__init__()
         self.name = "About"
         self.size = (500, 700)
         self.position = (0, 100)
         self.frame = np.full((*self.size, 4), (0, 255, 255, 255))
-        self.elements = []
+
+        self.elements += [
+            (0, 100, Text("The About App", font_size=3)),
+            (0, 200, Text("OpenAR v1.1 dev"))
+        ]
