570ffec7592d3b8ea3076f1cd99c9fd60514b4ec
usiqwerty <benlogin1@protonmail.com>
2024-07-11 14:05:15 +0500

    Handle exceptions while loading app

--- core/app_loader.py
+++ core/app_loader.py
@@ -1,8 +1,13 @@
 import importlib
 import json
+import traceback
 
-from gui.abstract.app import Application
 from device_config import root_path
+from gui.abstract.app import Application
+
+
+class AppNotLoaded(Exception):
+    pass
 
 
 def load_app(app_name) -> Application:
@@ -13,11 +18,21 @@ def load_app(app_name) -> Application:
     """
     manifest_path = '/'.join([root_path, *app_name.split('.'), "manifest.json"])
 
-    with open(manifest_path, encoding='utf-8') as f:
-        app_manifest: dict = json.load(f)
+    try:
+        with open(manifest_path, encoding='utf-8') as f:
+            app_manifest: dict = json.load(f)
+    except FileNotFoundError:
+        raise AppNotLoaded(f"Could not find manifest: {manifest_path}")
 
-    app_module = importlib.import_module(app_name)
+    try:
+        app_module = importlib.import_module(f"{app_name}.main")
+    except ModuleNotFoundError:
+        raise AppNotLoaded(f"Package has no 'main' module: {app_name}")
 
-    # TODO: передавать путь к папке с приложением
-    app_object: Application = app_module.App(app_manifest)
-    return app_object
+    try:
+        # TODO: передавать путь к папке с приложением
+        app_object: Application = app_module.App(app_manifest)
+        return app_object
+    except Exception as e:
+        print(traceback.format_exc())
+        raise AppNotLoaded(f"Exception happened while loading app: {e}") from e
